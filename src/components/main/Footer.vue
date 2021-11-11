774c8b6c935b160cfeae7fc34b10584f7db6b51b
Shinobi 3D <strelok0493@gmail.com>
2021-11-11 18:03:02 +0500

    job

--- src/components/main/Footer.vue
+++ src/components/main/Footer.vue
@@ -1,17 +1,11 @@
 <template>
-    <section class="container mx-auto flex mt-8 px-10 py-6">
-        <main>
-            <router-link class="block my-1" to="#">To`lov</router-link>
-            <router-link class="block my-1" to="#">Savol javoblar</router-link>
-            <router-link class="block my-1" to="#">Ish o`rinlari</router-link>
-        </main>
-    </section>
+	<aside class="bg-gray-50">
+		<section class="container mx-auto flex px-10 py-6 text-lg">
+			<main>
+				<router-link class="block my-1" to="#">To'lov</router-link>
+				<router-link class="block my-1" to="#">Savol javoblar</router-link>
+				<router-link class="block my-1" to="#">Ish o'rinlari</router-link>
+			</main>
+		</section>
+	</aside>
 </template>
-<script>
-export default {
-    
-}
-</script>
-<style>
-    
-</style>
--- src/views/main/HomePage.vue
+++ src/views/main/HomePage.vue
@@ -4,32 +4,19 @@
 		<!-- image block search-->
 		<section class="mainimage flex justify-center py-32 blur-sm bg-cover bg-center">
 			<main class="w-2/4 flex items-center bg-white pr-3">
-				<input
-					type="text"
-					id="searchiInp"
-					class="p-3 bg-white w-full"
-					placeholder="Nima buyurtma qilasiz?"
-				/>
+				<input type="text" id="searchiInp" class="p-3 bg-white w-full" placeholder="Nima buyurtma qilasiz?"/>
 				<label for="searchiInp" class="material-icons">search</label>
 			</main>
 		</section>
 		<!-- shop types -->
 		<section class="flex p-8 justify-center">
-			<main
-				class="p-5 py-3 mx-5 text-center shadow-xl border border-gray-100"
-				@click="getShops(null)"
-			>
+			<main class="p-5 py-3 mx-5 text-center shadow-xl border border-gray-100" @click="getShops(null)">
 				<div class="mb-1">
-					<img src="img" class="w-16 inline" />
+					<img src="http://127.0.0.1:8000/cafetypes/food.png" class="w-16 inline" />
 				</div>
 				<p>Hammasi</p>
 			</main>
-			<main
-				v-for="type in cafeTypes"
-				:key="type"
-				class="p-5 py-3 mx-5 text-center shadow-xl border border-gray-100"
-				@click="getShops(type.id)"
-			>
+			<main v-for="type in cafeTypes" :key="type" class="p-5 py-3 mx-5 text-center shadow-xl border border-gray-100" @click="getShops(type.id)">
 				<div class="mb-1">
 					<img :src="type.image" class="w-16 inline" />
 				</div>
@@ -37,8 +24,11 @@
 			</main>
 		</section>
 		<!-- shops -->
-		<section class="flex flex-wrap -mx-4">
-			<Card v-for="shop in shops" :key="shop.id" v-bind:shop="shop" class="w-1/4" />
+		<section v-if="shops != 0 && shops" class="flex flex-wrap -mx-4">
+			<Card v-for="shop in shops" :key="shop" v-bind:shop="shop" class="w-1/4" />
+		</section>
+		<section v-else class="text-3xl p-4 text-center">
+			Hech nima yo'q!
 		</section>
 	</aside>
 	<FooterSite />
@@ -57,22 +47,31 @@ export default {
 		return {
 			cafeTypes: null,
 			shops: null,
-			getCafeType: null
 		};
 	},
 	mounted() {
-		window.axios.get("/shoptype").then((res) => {
-			this.cafeTypes = res.data;
-		});
-		this.getShops(this.getCafeType)
+		this.shoptypes()
+		this.getShops()
 	},
-	methods:{
-		getShops(getCafeType){
-			window.axios.get("/shop/all").then((res) => {
-				this.shops = res.data;
+	methods: {
+		getShops(typeCafe = null) {
+			if(typeCafe == null){
+				window.axios.get("/shop/all").then((res) => {
+					this.shops = res.data;
+				});
+			}
+			else{
+				window.axios.get(`/shops/${typeCafe}`).then((res) => {
+					this.shops = res.data;
+				});
+			}
+		},
+		shoptypes(){
+			window.axios.get("/shoptype").then((res) => {
+				this.cafeTypes = res.data;
 			});
 		}
-	}
+	},
 };
 </script>
 <style>
